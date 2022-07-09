0051c00d3f3a5efbbde4ef98ede64672c2206de9
Paul Minh Huy Tran <32932124+pmtran5884@users.noreply.github.com>
2022-07-09 14:49:10 -0400

    added permutation test for model AUC values

--- code for figures/Figure_5_predictive_models.R
+++ code for figures/Figure_5_predictive_models.R
@@ -62,143 +62,63 @@ nullmod <- glm(Group ~
                na.action = na.omit,
                family = "binomial")
 
-### rand mod 1
-daisy$combined$Grouprand1<-sample(daisy$combined$Group)
-randmod1<-glm(Grouprand1 ~
-                Sex +
-                FDR +
-                HLA_risk +
-                Draw_Age +
-                Cluster1 +
-                Cluster2 +
-                Cluster3 +
-                Cluster4 +
-                Cluster5 +
-                Cluster6 +
-                Cluster7 +
-                Cluster8 +
-                Cluster9 +
-                Cluster10 +
-                Cluster11,
-              data = daisy$combined,
-              subset = which(daisy$combined$Grouprand1!="Non-progressor"),
-              na.action = na.omit,
-              family = "binomial")
+#randmod function
+randmodAUC<-function(mydata=daisy$combined){
+  mydata$Grouprand1<-sample(mydata$Group)
+  randmod1<-glm(Grouprand1 ~
+                  Sex +
+                  FDR +
+                  HLA_risk +
+                  Draw_Age +
+                  Cluster1 +
+                  Cluster2 +
+                  Cluster3 +
+                  Cluster4 +
+                  Cluster5 +
+                  Cluster6 +
+                  Cluster7 +
+                  Cluster8 +
+                  Cluster9 +
+                  Cluster10 +
+                  Cluster11,
+                data = mydata,
+                subset = which(mydata$Grouprand1!="Non-progressor"),
+                na.action = na.omit,
+                family = "binomial")
 
-#2
-daisy$combined$Grouprand2<-sample(daisy$combined$Group)
-randmod2<-glm(Grouprand2 ~
-                Sex +
-                FDR +
-                HLA_risk +
-                Draw_Age +
-                Cluster1 +
-                Cluster2 +
-                Cluster3 +
-                Cluster4 +
-                Cluster5 +
-                Cluster6 +
-                Cluster7 +
-                Cluster8 +
-                Cluster9 +
-                Cluster10 +
-                Cluster11,
-              data = daisy$combined,
-              subset = which(daisy$combined$Grouprand2!="Non-progressor"),
-              na.action = na.omit,
-              family = "binomial")
-#3
-daisy$combined$Grouprand3<-sample(daisy$combined$Group)
-randmod3<-glm(Grouprand3 ~
-                Sex +
-                FDR +
-                HLA_risk +
-                Draw_Age +
-                Cluster1 +
-                Cluster2 +
-                Cluster3 +
-                Cluster4 +
-                Cluster5 +
-                Cluster6 +
-                Cluster7 +
-                Cluster8 +
-                Cluster9 +
-                Cluster10 +
-                Cluster11,
-              data = daisy$combined,
-              subset = which(daisy$combined$Grouprand3!="Non-progressor"),
-              na.action = na.omit,
-              family = "binomial")
-
-#4
-daisy$combined$Grouprand4<-sample(daisy$combined$Group)
-randmod4<-glm(Grouprand4 ~
-                Sex +
-                FDR +
-                HLA_risk +
-                Draw_Age +
-                Cluster1 +
-                Cluster2 +
-                Cluster3 +
-                Cluster4 +
-                Cluster5 +
-                Cluster6 +
-                Cluster7 +
-                Cluster8 +
-                Cluster9 +
-                Cluster10 +
-                Cluster11,
-              data = daisy$combined,
-              subset = which(daisy$combined$Grouprand4!="Non-progressor"),
-              na.action = na.omit,
-              family = "binomial")
-
-#5
-daisy$combined$Grouprand5<-sample(daisy$combined$Group)
-randmod5<-glm(Grouprand5 ~
-                Sex +
-                FDR +
-                HLA_risk +
-                Draw_Age +
-                Cluster1 +
-                Cluster2 +
-                Cluster3 +
-                Cluster4 +
-                Cluster5 +
-                Cluster6 +
-                Cluster7 +
-                Cluster8 +
-                Cluster9 +
-                Cluster10 +
-                Cluster11,
-              data = daisy$combined,
-              subset = which(daisy$combined$Grouprand5!="Non-progressor"),
-              na.action = na.omit,
-              family = "binomial")
-
-#ROC plot
-test_roc<-plot(pROC::roc(sample(fastDummies::dummy_cols(daisy$combined$Grouprand1[daisy$combined$Grouprand1!="Non-progressor"])[,4]),
-                         predict(randmod1),ci=T),print.auc=T,col="grey")
-test_roc<-plot(pROC::roc(sample(fastDummies::dummy_cols(daisy$combined$Grouprand2[daisy$combined$Grouprand2!="Non-progressor"])[,4]),
-                         predict(randmod2),ci=T),print.auc=T,add=T,col="grey")
-test_roc<-plot(pROC::roc(sample(fastDummies::dummy_cols(daisy$combined$Grouprand3[daisy$combined$Grouprand3!="Non-progressor"])[,4]),
-                         predict(randmod3),ci=T),print.auc=T,add=T,col="grey")
-test_roc<-plot(pROC::roc(sample(fastDummies::dummy_cols(daisy$combined$Grouprand4[daisy$combined$Grouprand4!="Non-progressor"])[,4]),
-                         predict(randmod4),ci=T),print.auc=T,add=T,col="grey")
-test_roc<-plot(pROC::roc(sample(fastDummies::dummy_cols(daisy$combined$Grouprand5[daisy$combined$Grouprand5!="Non-progressor"])[,4]),
-                         predict(randmod5),ci=T),print.auc=T,add=T,col="grey")
+  #ROC plot
+  test_roc<-pROC::roc(fastDummies::dummy_cols(mydata$Grouprand1[mydata$Grouprand1!="Non-progressor"])[,4],
+                      predict(randmod1))
+  as.numeric(test_roc$auc)
+}
 
 
+#Calculate ROC for models
+null_roc<-plot(pROC::smooth(pROC::roc(fastDummies::dummy_cols(daisy$combined$Group[daisy$combined$Group!="Non-progressor"])[,4],
+                         predict(nullmod),ci=T)),print.auc=T,add=F,col="black")
+full_roc<-plot(pROC::smooth(pROC::roc(fastDummies::dummy_cols(daisy$combined$Group[daisy$combined$Group!="Non-progressor"])[,4],
+                                predict(fullmod),ci=T)),print.auc=T,add=T,col="red",print.auc.y=0.4)
+cl8_roc<-plot(pROC::smooth(pROC::roc(fastDummies::dummy_cols(daisy$combined$Group[daisy$combined$Group!="Non-progressor"])[,4],
+                                predict(cl8mod),ci=T)),print.auc=T,add=T,col="blue",print.auc.y=0.3)
 
-test_roc<-plot(pROC::roc(fastDummies::dummy_cols(daisy$combined$Group[daisy$combined$Group!="Non-progressor"])[,4],
-                         predict(nullmod),ci=T),print.auc=T,add=T,col="black")
-test_roc<-plot(pROC::roc(fastDummies::dummy_cols(daisy$combined$Group[daisy$combined$Group!="Non-progressor"])[,4],
-                                predict(fullmod),ci=T),print.auc=T,add=T,col="red",print.auc.y=0.4)
-test_roc<-plot(pROC::roc(fastDummies::dummy_cols(daisy$combined$Group[daisy$combined$Group!="Non-progressor"])[,4],
-                                predict(cl8mod),ci=T),print.auc=T,add=T,col="blue",print.auc.y=0.3)
+#Calculate q val for each model
+AUCs<-replicate(10000,randmodAUC(mydata=daisy$combined))
+
+qval<-function(randAUCs=AUCs,testAUC=full_roc$auc){
+qval<-1-(sum(randAUCs<as.numeric(testAUC))/10000)
+qval
+}
+
+qval(testAUC = full_roc$auc)
+qval(testAUC = cl8_roc$auc)
+qval(testAUC = null_roc$auc)
 
 #LRT to compare models
-
 lmtest::lrtest(fullmod, nullmod)
 lmtest::lrtest(cl8mod, nullmod)
 lmtest::lrtest(cl8mod, fullmod)
+
+# test_roc<-plot(pROC::smooth(pROC::roc(fastDummies::dummy_cols(daisy$combined$Grouprand1[daisy$combined$Grouprand1!="Non-progressor"])[,4],
+#                          predict(randmod1),ci=T)),print.auc=T,col="grey")
+
+
